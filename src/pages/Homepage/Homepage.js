7182ddda3141e00c1974dd3ea2b976985e737892
Winters67 <emmanuel.vbg@gmail.com>
2023-07-04 14:56:22 +0200

    refactorisation fichier & dossier

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import styles from "./Content.module.scss";
-import Recipe from "./Recipe";
-import { data } from "../data/recipes";
+import styles from "./Homepage.module.scss";
+import Recipe from "./components/Recipe/Recipe";
+import { data } from "../../data/recipes";
 
 export default function Content() {
   const recipes = data;
